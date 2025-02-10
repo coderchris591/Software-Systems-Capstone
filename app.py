a7752cb6bd176e68359da7c40c7fc7ba0294978e
Chris Martinez <chmartinez2014@gmail.com>
2025-02-10 12:13:11 -0600

    added database, user registration/login

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 
 
-from flask import Flask, render_template, request
-import requests, my_secrets
+from flask import Flask, render_template, request, redirect, url_for, flash, session, g
+import requests, my_secrets, db
+from db import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from geopy.geocoders import Nominatim
+import os
+import functools
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'work4gov.sqlite'),
+    )
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+db.init_app(app)
+
+
 geolocator = Nominatim(user_agent="work4gov")
             
     
@@ -16,7 +33,132 @@ headers = {
 }
 
 
-@app.route("/", methods=['POST', 'GET'])
+
+
+
+
+
+
+
+
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("login"))
+
+        flash(error)
+
+    return render_template('register.html')
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('search'))
+
+        flash(error)
+
+    return render_template('login.html')
+
+
+
+
+
+
+
+@app.before_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
+
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route("/search", methods=['POST', 'GET'])
+@login_required
 def search():
 
     # INFO from: https://developer.usajobs.gov/api-reference/get-api-search
